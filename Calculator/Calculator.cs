d41903c53f808689d0db446c370535814fa022d8
sridevimanasa <msmanasac033@gmail.com>
2022-12-15 23:55:35 +0530

    [manasa] Add functionality for adding two numbers

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Calculator
+    {
+        public static int Add(int firstNumber , int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Please enter the first number");
+            string firstNumber = Console.ReadLine();
+            Console.WriteLine("Please enter the second number");
+            string secondNumber = Console.ReadLine();
+            int first = Convert.ToInt32(firstNumber);
+            int second = Convert.ToInt32(secondNumber);
+            int sum = Calculator.Add(first, second);
+            Console.WriteLine("sum = " + sum);
+        }
+    }
+}
